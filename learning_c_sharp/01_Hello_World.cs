f089d4ddc36c598c3ef38fa8e113f4c6958ed49d
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2021-02-28 17:23:52 +0300

    Update 01_Hello_World.cs

--- learning_c_sharp/01_Hello_World.cs
+++ learning_c_sharp/01_Hello_World.cs
@@ -1,1 +1,13 @@
-// Привет Мир!!!
+using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello world!!!");
+        }
+    }
+}
+
