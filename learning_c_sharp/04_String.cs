a30816a608a4ae8ecc29fb865a2458680753e526
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2021-04-12 20:17:01 +0300

    Update 04_String.cs

--- learning_c_sharp/04_String.cs
+++ learning_c_sharp/04_String.cs
@@ -5,9 +5,19 @@ namespace String
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            
+            // Строка - это последовательность символов. 
+            string myString = "Hello world";
+
+            // "+" - это символ приписывания, одной строки к другой. 
+            string s = "Hello " + " " + " world";
+
+            // Так-же, можно обращаться к отдельным символам
+            char c = myString[1]; // 'e' - нумирация символов с нуля. 
+            char myChar = 'e'; // Одинарные ковычки используются для символов. Двойные для строк. 
+
+
         }
     }
 }
